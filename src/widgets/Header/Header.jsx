93ac53828086c5ab646a714f5f194d01277007d4
mykolamieshkov-web <mykolamieshkov@gmail.com>
2026-05-26 11:22:02 +0300

    upload test

--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = ( ) => { // вызов функции
          <div className="header__container">
                <div className="header__navigation">
                  <div className="header__logo">
-                       <h1>hello world!</h1>
+                       <h1>hello this is Myk's portfolio!</h1>
                  </div>
                  </div>
          </div>
