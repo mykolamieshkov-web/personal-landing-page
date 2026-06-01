d4154ef7f21bf78505a7497d4d75e771a45de6f1
mykolamieshkov-web <mykolamieshkov@gmail.com>
2026-06-01 18:09:01 +0300

    test upload

--- src/widgets/Footer/Footer.jsx
+++ src/widgets/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container">
-        Footer Content
+        Footer Content Here
       </div>
     </footer>
   );
